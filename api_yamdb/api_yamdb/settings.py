0cae1518a62e333ff286e6929c2ede30fa509637
Андрей Стрельников <andre33_3@rambler.ru>
2022-07-26 19:56:04 +0600

    sekret_key

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,9 @@ load_dotenv()
 # env_path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs"
+)
 
 DEBUG = False
 

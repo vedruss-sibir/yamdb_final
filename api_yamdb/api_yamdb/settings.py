38fd6b5756b603a13948fd91b2d2dc069e736fcc
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-03 08:42:16 +0600

    redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,13 +1,46 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf.urls import url
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cats API",
+        default_version="v1",
+        description="Документация для приложения cats проекта Kittygram",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="admin@kittygram.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path(
-        "redoc/",
-        TemplateView.as_view(template_name="api/redoc.html"),
-        name="redoc",
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
     ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+    #    path(
+    #        "redoc/",
+    #        TemplateView.as_view(template_name="api/redoc.html"),
+    #        name="redoc",
+    #    ),
 ]

57d7bff162693df7f400cb43a663b3c91283f201
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-03 09:18:05 +0600

    re

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls import url
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,14 +32,5 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    url(
-        r"^redoc/$",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
-    #    path(
-    #        "redoc/",
-    #        TemplateView.as_view(template_name="api/redoc.html"),
-    #        name="redoc",
-    #    ),
+    path("redoc/", TemplateView.as_view(template_name="api/redoc.html")),
 ]

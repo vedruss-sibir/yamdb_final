1ec767df41ddfba82718e67cb01c934bf9f9d58b
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-03 08:47:43 +0600

    red2

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+
+# from django.views.generic import TemplateView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf.urls import url
-
 
 schema_view = get_schema_view(
     openapi.Info(

c90e55fceaa383419049dfc3302cdf23ff85622b
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-04 08:10:31 +0600

    haker2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -107,10 +107,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, "static/")),
-    ("redoc", "/app/templates/"),
-]
+# STATICFILES_DIRS = [
+#   (os.path.join(BASE_DIR, "static/")),
+#   ("redoc", "/app/templates/"),
+# ]
 
 AUTH_USER_MODEL = "users.User"
 

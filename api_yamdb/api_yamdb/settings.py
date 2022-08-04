1382048115b527c0ad3ac8fd5febe2aad60202e8
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-04 11:33:08 +0600

    m

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -70,7 +70,8 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-        "HOST": os.getenv("DB_HOST", default="db"),
+        #     "HOST": os.getenv("DB_HOST", default="db"),
+        "HOST": os.getenv("localhost"),
         "PORT": os.getenv("DB_PORT", default="5432"),
     }
 }

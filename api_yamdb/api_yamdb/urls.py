9deb0a5e1a978b552f3bbe2bf9184ef5a7ef832b
Андрей Стрельников <andre33_3@rambler.ru>
2022-08-05 10:44:11 +0600

    m5

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path(
         "redoc/",
-        TemplateView.as_view(template_name="api_yamdb\static\redoc.html"),
+        TemplateView.as_view(template_name="/app/templates/redoc.html"),
         name="redoc",
     ),
 ]
